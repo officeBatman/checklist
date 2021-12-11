017cba6b754a74ccb9bdd10a75bdd61ecd041784
Yonatan Reicher <yony252525@gmail.com>
2021-12-11 18:42:18 +0200

    Added persistance

--- src/Main.elm
+++ src/Main.elm
@@ -10,9 +10,11 @@ import Element.Font as Font
 import Element.Input as Input
 import Html.Events.Extra exposing (onEnter)
 
+import Json.Encode as E
+import Json.Decode as D
+import Json.Decode.Extra as DExtra
 
-main : Program () Model Msg
-main = Browser.sandbox { init = init, view = view, update = update }
+import Ports exposing (setStorage)
 
 
 type alias Model =
@@ -21,39 +23,85 @@ type alias Model =
     , next_task_text: String -- Text for the next task
     }
 
+encode : Model -> E.Value
+encode model = 
+    E.object
+        [ ("tasks", E.dict String.fromInt encodeTask model.tasks)
+        , ("next_task_id", E.int model.next_task_id)
+        , ("next_task_text", E.string model.next_task_text)
+        ]
+
+decode : D.Decoder Model
+decode =
+    D.map3 Model
+        (D.field "tasks" (DExtra.dict2 D.int decodeTask))
+        (D.field "next_task_id" D.int)
+        (D.field "next_task_text" D.string)
+
 type alias Task =
     { text: String
     , checked: Bool
     }
 
-init : Model
-init = { tasks = Dict.empty, next_task_id = 0, next_task_text = "" }
+encodeTask: Task -> E.Value
+encodeTask { text, checked } =
+    E.object
+        [ ("text", E.string text)
+        , ("checked", E.bool checked)
+        ]
+
+decodeTask : D.Decoder Task
+decodeTask =
+    D.map2 Task
+        (D.field "text" D.string)
+        (D.field "checked" D.bool)
 
 type Msg = AddTask Task | TypedNextTask String | CheckTask Int Bool | RemoveTask Int
 
-update : Msg -> Model -> Model
+main : Program D.Value Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+init : D.Value -> (Model, Cmd Msg)
+init encoded =
+    case D.decodeValue decode encoded of
+        Ok model -> (model, Cmd.none)
+
+        Err e -> Debug.log "Error" e |> \_ -> ({ tasks = Dict.empty, next_task_id = 0, next_task_text = "" }, Cmd.none)
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
     case msg of
         AddTask task ->
             -- Check that task text is not empty
-            if task.text == "" then model else
+            if task.text == "" then (model, Cmd.none) else
                 -- Add the task to the list of tasks
                 let tasks = Dict.insert model.next_task_id task model.tasks
                     next_task_id = model.next_task_id - 1 -- -1 so tasks are added from the top
                 in
-                { model | tasks = tasks, next_task_id = next_task_id, next_task_text = "" }
+                { model | tasks = tasks, next_task_id = next_task_id, next_task_text = "" } |> modelAndSave
 
-        TypedNextTask task -> { model | next_task_text = task }
+        TypedNextTask task -> { model | next_task_text = task } |> modelAndSave
 
         CheckTask index checked ->
             case model.tasks |> Dict.get index of
                 Just task ->
-                    { model | tasks = Dict.insert index { task | checked = checked } model.tasks }
+                    { model | tasks = Dict.insert index { task | checked = checked } model.tasks } |> modelAndSave
 
-                Nothing -> model
+                Nothing -> (model, Cmd.none)
 
         RemoveTask index ->
-            { model | tasks = Dict.remove index model.tasks }
+            { model | tasks = Dict.remove index model.tasks } |> modelAndSave
+
+modelAndSave : Model -> (Model, Cmd Msg)
+modelAndSave model = (model, model |> encode |> setStorage)
 
 colors =
     { dark = El.rgb255 25 28 26
